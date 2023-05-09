2c4a516a5da8bafcf40720c6c82b6b1d197371e4
kerem <77764573+yahya-tamur@users.noreply.github.com>
2023-05-09 15:57:10 -0700

    added a lot

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 ```cargo run us-citizenship.txt```
 
-This will probably only work on a linux terminal.
+This only works on a linux terminal.
 
-While parsing the flashcard file, ignores spaces, then new lines following "Q:" and "A:".
-Ignores whitespace at the end of a question or an answer.
-Questions and answers are printed directly to the terminal, so they should be line-wrapped if necessary and not contain symbols that might cause problems.
+In the flashcard file, the first line contains the card width. Questions and
+answers should be line wrapped to fit this width.
+The questions can have optional numberings or namings, before
+":". These are ignored. Spaces, then newlines after "Q ... :" and "A:", and
+whitespace characters at the end of a question or answer are also ignored.
+
+Questions and answers are printed directly to the terminal, so they should't
+contain symbols that might cause problems.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![feature(iter_intersperse)]
 use rand::seq::SliceRandom;
 use regex::Regex;
 use std::io::Write;
@@ -5,32 +6,55 @@ use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 
-fn parse(input: &str) -> Vec<(String, String)> {
+fn parse(input: &str) -> (Vec<(String, String)>, usize) {
+    let card_width: usize = input
+        .chars()
+        .take_while(|x| x.is_numeric())
+        .collect::<String>()
+        .parse()
+        .unwrap();
+    let second_line = input.find('\n').unwrap();
+    let (_, input) = input.split_at(second_line + 1);
+
     let r = Regex::new(
         r"(?x)
-        (Q:|)\ *\n*(?P<Q>(.|\n)*?)
-        A: *\n*(?P<A>(.|\n)*?)(Q:|\z)
+        (Q.*?:)?\ *\n*(?P<Q>(.|\n)*?)
+        A:\ *\n*(?P<A>(.|\n)*?)((Q.*?:)|\z)
         ",
     )
     .unwrap();
-    fn part(caps: &regex::Captures, name: &str) -> String {
-        "  ".to_string()
-            + &caps
-                .name(name)
-                .expect("parse error")
-                .as_str()
-                .trim_end()
-                .replace('\n', "\r\n  ")
+    fn part(caps: &regex::Captures, name: &str, card_width: usize) -> String {
+        caps.name(name)
+            .expect("parse error")
+            .as_str()
+            .trim_end()
+            .lines()
+            .map(|line| format!("  │  {:card_width$}  │  ", line))
+            .intersperse("\r\n".to_string())
+            .collect()
     }
 
-    r.captures_iter(input)
-        .map(|caps| (part(&caps, "Q"), part(&caps, "A")))
-        .collect()
+    (
+        r.captures_iter(input)
+            .map(|caps| (part(&caps, "Q", card_width), part(&caps, "A", card_width)))
+            .collect(),
+        card_width,
+    )
+}
+
+struct State {
+    flashcards: Vec<(String, String)>,
+    card_width: usize,
+    starred: Vec<bool>,
+    num_starred: usize,
+    indices: Vec<usize>,
+    i: usize,
+    on: bool,
 }
 
 fn main() {
     let mut rng = rand::thread_rng();
-    let mut flashcards = parse(
+    let (flashcards, card_width) = parse(
         &std::fs::read_to_string(
             std::env::args()
                 .nth(1)
@@ -38,58 +62,106 @@ fn main() {
         )
         .expect("couldn't read file"),
     );
-    flashcards.shuffle(&mut rng);
+    let n = flashcards.len();
 
-    let mut i = 0;
-    let mut on = false;
+    let mut s = State {
+        flashcards,
+        card_width,
+        starred: vec![false; n],
+        num_starred: 0,
+        indices: (0..100).collect(),
+        i: 0,
+        on: false,
+    };
+
+    s.indices.shuffle(&mut rng);
 
     print!("{}", termion::cursor::Hide);
 
     let mut stdout = std::io::stdout().into_raw_mode().unwrap();
 
-    fn print_screen(flashcards: &Vec<(String, String)>, i: usize, on: bool) {
+    fn print_screen(s: &State) {
         print!("{}{}", termion::cursor::Goto(1, 1), termion::clear::All);
 
-        if i == flashcards.len() {
-            print!("\r\n  finished! press left to go back, right to reshuffle and start over.");
-        } else {
+        if s.i == s.indices.len() {
             print!(
-                "\r\n  Use the arrow keys to move around, and press q to exit.\r\n\r\n  ({}/{})\r\n\r\n",
-                i + 1,
-                flashcards.len(),
+                "\r\n  finished! press left to go back, enter to reshuffle and start over.\r\n\r\n"
             );
-            print!("{}\r\n\r\n", flashcards[i].0);
-            if on {
-                print!("{}\r\n\r\n", flashcards[i].1);
+            print!(
+                "  {} out of {} starred.\r\n\r\n",
+                s.num_starred,
+                s.flashcards.len()
+            );
+            print!("  ({}/{})\r\n\r\n", s.indices.len(), s.indices.len(),);
+            if !s.on {
+                print!("  will study starred cards only. press up or down to flip\r\n\r\n");
+            } else {
+                print!("  will study all cards. press up or down to flip\r\n\r\n");
             }
+        } else {
+            print!("\r\n  use arrow keys to move, enter to star or unstar, and q to exit.\r\n\r\n");
+            print!(
+                "  {} out of {} starred.\r\n\r\n",
+                s.num_starred,
+                s.flashcards.len()
+            );
+            print!(
+                "  ({}/{}) {}\r\n\r\n",
+                s.i + 1,
+                s.indices.len(),
+                if s.starred[s.indices[s.i]] {
+                    "Starred"
+                } else {
+                    "Not Starred"
+                }
+            );
+            print!("  ┌──{:─<1$}──┐\r\n", "", s.card_width);
+            print!("{}\r\n", s.flashcards[s.indices[s.i]].0);
+            if s.on {
+                print!("  ├──{:─<1$}──┤\r\n", "", s.card_width);
+                print!("{}\r\n", s.flashcards[s.indices[s.i]].1);
+            }
+            print!("  └──{:─<1$}──┘\r\n\r\n", "", s.card_width);
         }
     }
 
-    print_screen(&flashcards, i, on);
+    print_screen(&s);
     stdout.flush().unwrap();
 
     for c in std::io::stdin().keys() {
         match c.unwrap() {
-            Key::Char('q') | Key::Ctrl('c') => break,
-            Key::Left => {
-                if i > 0 {
-                    i -= 1;
-                    on = false;
-                }
-            }
-            Key::Right => {
-                if i == flashcards.len() {
-                    flashcards.shuffle(&mut rng);
-                    i = 0;
+            Key::Char('\n') => {
+                if s.i == s.indices.len() {
+                    if !s.on {
+                        s.indices = (0..100).filter(|i| s.starred[*i]).collect();
+                    } else {
+                        s.indices = (0..100).collect();
+                    }
+                    s.indices.shuffle(&mut rng);
+                    s.i = 0;
+                    s.on = false;
                 } else {
-                    i += 1;
+                    if s.starred[s.indices[s.i]] {
+                        s.num_starred -= 1;
+                    } else {
+                        s.num_starred += 1;
+                    }
+                    s.starred[s.indices[s.i]] ^= true;
                 }
-                on = false;
             }
-            Key::Down | Key::Up => on = !on,
+            Key::Char('q') | Key::Ctrl('c') => break,
+            Key::Left if s.i > 0 => {
+                s.i -= 1;
+                s.on = false;
+            }
+            Key::Right if s.i < s.indices.len() => {
+                s.i += 1;
+                s.on = false;
+            }
+            Key::Down | Key::Up => s.on ^= true,
             _ => {}
         }
-        print_screen(&flashcards, i, on);
+        print_screen(&s);
         stdout.flush().unwrap();
     }
     print!("{}", termion::cursor::Show);
